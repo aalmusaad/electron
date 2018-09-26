1a030c899fb805604f074be5ab49c5e8e01e46bf
John Kleinschmidt <jkleinsc@github.com>
2018-09-26 11:10:20 -0400

    ci: Trigger an arm test on VSTS after CircleCI build

--- script/dbus_mock.py
+++ script/dbus_mock.py
@@ -10,21 +10,29 @@ from dbusmock import DBusTestCase
 from lib.config import is_verbose_mode
 
 def stop():
+    print('In dbus_mock stop')
     DBusTestCase.stop_dbus(DBusTestCase.system_bus_pid)
+    print('In dbus_mock stop, stopped system bus')
     DBusTestCase.stop_dbus(DBusTestCase.session_bus_pid)
+    print('In dbus_mock stop, stopped session bus')
 
 def start():
-    log = sys.stdout if is_verbose_mode() else open(os.devnull, 'w')
-
+    log = sys.stdout
+    print('In dbus_mock start')
     DBusTestCase.start_system_bus()
+    print('In dbus_mock start, started system bus')
     DBusTestCase.spawn_server_template('logind', None, log)
-
+    print('In dbus_mock start, spawned logind')
     DBusTestCase.start_session_bus()
+    print('In dbus_mock start, started session bus')
     DBusTestCase.spawn_server_template('notification_daemon', None, log)
+    print('In dbus_mock start, spawned notification_daemon')
 
 if __name__ == '__main__':
     start()
     try:
+        print 'About to call {}'.format(sys.argv)
         subprocess.check_call(sys.argv[1:])
+        print 'Done calling {}'.format(sys.argv)
     finally:
         stop()
